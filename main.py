c76a53dd6782ce114189760c8e705551d500e767
isOdin <ivanbildagin@mail.ru>
2024-10-08 23:54:47 +0300

    Появление/исчезание стрелок по времени

--- main.py
+++ main.py
@@ -1,40 +1,71 @@
 import pygame
-import Teacher
+import MessageClass
 
 pygame.init()
 
 screen = pygame.display.set_mode((1100,800))
+clock = pygame.time.Clock()
 btnEventId = {"RIGHT": 1073741903, "LEFT": 1073741904, "DOWN": 1073741905, "UP":1073741906}
 
+WAIT_NEW_MSG_TIME = 3
+WAIT_ANSWER_TIME = 5
+FPS = 60
+
 def rhythm_game_start():
-    rhythm_line = ["RIGHT", "LEFT", "DOWN", "DOWN", "UP"] # список клавиш, которые надо нажать
+    rhythmLine = ["RIGHT", "LEFT", "DOWN", "DOWN", "UP"] # список клавиш, которые надо нажать
     task_num = 0
+
+    msg = MessageClass.Message(screen)
+    msg.show_Next_Symbol(rhythmLine[task_num])
+
+    tick_update_msg = pygame.time.get_ticks()
     
-    teacher = Teacher.Teacher(screen)
+    is_running = True
+    while is_running:
 
-    is_Rhythm_Game_Running = 1
-    while is_Rhythm_Game_Running:
-        pygame.display.update()
-
-        teacher.show_Next_Symbol(rhythm_line[task_num])
+        clock.tick(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                is_Rhythm_Game_Running = 0
+                is_running = False
                 pygame.quit()
 
             #Обработка нажатых клавиш
             if event.type == pygame.KEYDOWN:
-                if event.key == btnEventId[ rhythm_line[task_num] ]: # проверка на условие нажатая клавиша == ожидаемой клавише
+                if not(msg.updating) and event.key == btnEventId[ rhythmLine[task_num] ]: # проверка на условие нажатая клавиша == ожидаемой клавише
                     print("Cool")
-                    task_num+=1
 
-                    teacher.delete_previous_Symbol()
-                    teacher.show_Next_Symbol(rhythm_line[task_num])
-
+                    msg.updating = True
+                    ticks_next_msg = pygame.time.get_ticks()
+                    
+                    msg.show_Next_Symbol("RIGHT_ANSWER")
                 else:
-                    print("Bad!")
-                    #task_num+=1
+                    msg.show_Next_Symbol("WRONG_ANSWER")
+                    # -health
+        try:
+            if (pygame.time.get_ticks() - ticks_next_msg)//1000 == WAIT_NEW_MSG_TIME:
+                task_num+=1
 
+                msg.updating = False
+                ticks_next_msg = None
+                tick_update_msg = pygame.time.get_ticks()
+                
+                msg.show_Next_Symbol(rhythmLine[task_num])
+
+        except:
+            pass
+
+        try:
+            if (pygame.time.get_ticks() - tick_update_msg)//1000 == WAIT_ANSWER_TIME:
+                msg.updating = True
+                tick_update_msg = None
+                ticks_next_msg = pygame.time.get_ticks()
+                
+                screen.fill((0,0,0))
+        except:
+            pass
+
+
+        pygame.display.update()
 
 rhythm_game_start()
