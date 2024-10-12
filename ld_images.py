dd8c694f35d2e0a1d16ef53953673b20c7626843
DerasGertony <denis2006mareev@gmail.com>
2024-10-12 13:20:36 +0300

    good start

---
+++ ld_images.py
@@ -0,0 +1,12 @@
+import sys
+import pygame
+
+
+#backs
+startback = pygame.image.load('images/stbc.jpg')
+
+
+#buttons
+sb1 = pygame.image.load('images/stlv1.png')
+sb2= pygame.image.load('images/stlv2.png')
+sb3= pygame.image.load('images/stlv3.png')
