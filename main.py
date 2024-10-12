ee3fe1c149ba0116e5219e423343cb638b4dc299
DerasGertony <denis2006mareev@gmail.com>
2024-10-12 13:20:36 +0300

    good start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports
 import sys
 import pygame
+from ld_images import *
 
 # Configuration
 pygame.init()
@@ -13,48 +14,65 @@ font = pygame.font.SysFont('Arial', 40)
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, pos, text, font,fc):
+    def __init__(self, pos, text, font,fc, image, size, where):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((100, 50))
-        self.image.fill((0,100,0))
+        image = pygame.transform.scale(image, size)
+        self.image = image
         self.rect = self.image.get_rect()
         self.rect.center = (pos[0], pos[1])
         self.buttonText = font.render(text, True, fc)
-        pygame.draw.rect(self.image,(0,100,0),pygame.Rect(0, 0, width, height))
-        self.image.blit(self.buttonText, [
-            self.rect.width / 2 - self.buttonText.get_rect().width / 2,
-            self.rect.height / 2 - self.buttonText.get_rect().height / 2
-        ])
+        self.where = where
+
     def use(self):
-        return 1
+        return self.where
 
 
 class BackGround(pygame.sprite.Sprite):
-    def __init__(self, path):
+    def __init__(self, image):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path)
+        image = pygame.transform.scale(image, (width, height))
+        self.image = image
         self.rect = self.image.get_rect(center=(width / 2, height / 2))
 
+
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, x, y, path):
+    def __init__(self, x, y, image):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path)
+        self.image = image
         self.rect = self.image.get_rect(center=(x, y))
 
 
 use_sprites = pygame.sprite.Group()  # interactive sprites
-
+dec_sprites = pygame.sprite.Group()
 # starting screen
-st1 = Button([width / 2, height / 3], 'brat', font, (0, 0, 0))
-st2 = Button([width/2, height/2], 'brat', font, (0,0,0))
-st3 = Button([width/2, height/1.5], 'brat', font, (0,0,0))
+back = BackGround(startback)
+st1 = Button([width / 4, height / 2 + 50], 's', font, (0, 0, 0),sb1, (808, 152), (1,1))
+st2 = Button([width/4 +10, height/2 +200], '', font, (0,0,0),sb2,(804, 151), (2,1))
+st3 = Button([width/4, height/2 + 350], '', font, (0,0,0),sb3,(800, 150), (3,1))
 use_sprites.add(st1)
 use_sprites.add(st2)
 use_sprites.add(st3)
+dec_sprites.add(back)
+
+butns = []
+backs = []
+heroes = []
+
+
+def level(num):
+    return 1
+
+
+
+
+
+
+
+
 
 #dialogue window
-back = 0
-hero = 0
+#back = BackGround()
+#hero = Hero()
 
 clock = pygame.time.Clock()
 while True:
@@ -67,10 +85,11 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for sprite in use_sprites:
                 if sprite.rect.collidepoint(event.pos):
-                    sprite.use()
-
+                    level(sprite.use())
+    dec_sprites.update()
     use_sprites.update()
     screen.fill((0, 0, 0))
+    dec_sprites.draw(screen)
     use_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(60)
