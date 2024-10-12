4d7a3ac21c5cc48a8b7c8d430ac9de794ee2587c
Денис Артемьев <daartemev@edu.hse.ru>
2024-10-12 16:45:59 +0300

    added images
    Add Heroes and BackGrounds (bus, home, outside) files

--- ld_images.py
+++ ld_images.py
@@ -2,20 +2,45 @@ import sys
 import pygame
 
 
-#backs
+# Backs
 startback = pygame.image.load('images/stbc.jpg')
 bus = pygame.image.load('Pictures/backgrounds/bus/3.jpg')
 home = pygame.image.load('images/stbc.jpg')
 bed = pygame.image.load('images/stbc.jpg')
 
-#buttons
+# Buttons
 sb1 = pygame.image.load('images/stlv1.png')
 sb2= pygame.image.load('images/stlv2.png')
 sb3= pygame.image.load('images/stlv3.png')
 
 
-#dg window
+# dg window
 dg = pygame.image.load('images/dgf.png')
 ex = pygame.image.load('images/btexit.png')
 sk = pygame.image.load('images/btfr.png')
 bc = pygame.image.load('images/btbc.png')
+
+
+# Heroes
+gm1 = pygame.image.load('Pictures/characters/grandmother_1/main_1.png')
+gm2 = pygame.image.load('Pictures/characters/grandmother_2/main.png')
+qr = pygame.image.load('Pictures/characters/quadrober/main.png')
+
+# BackGrounds
+  # BUS
+bus1 = pygame.image.load('Pictures/backgrounds/bus/1.jpg') # idk как их назвать по другому
+bus2 = pygame.image.load('Pictures/backgrounds/bus/2.jpg')
+bus3 = pygame.image.load('Pictures/backgrounds/bus/3.jpg')
+
+  # HOME
+alarm = pygame.image.load('Pictures/backgrounds/home/alarm.png')
+kitchen = pygame.image.load('Pictures/backgrounds/home/kitchen.png')
+phone = pygame.image.load('Pictures/backgrounds/home/phone.png')
+mainbg = pygame.image.load('Pictures/backgrounds/home/main_back.png')
+
+  # OUTSIDE
+CrowdedModernStreet = pygame.image.load('Pictures/backgrounds/outside/3.jpg')
+CrowdedStreet = pygame.image.load('Pictures/backgrounds/outside/5.jpg')
+DesertedDarknessStreet = pygame.image.load('Pictures/backgrounds/outside/4.jpg')
+DesertedSunnyStreet = pygame.image.load('Pictures/backgrounds/outside/1.jpg')
+DesertedRadiantStreet = pygame.image.load('Pictures/backgrounds/outside/2.jpg')
