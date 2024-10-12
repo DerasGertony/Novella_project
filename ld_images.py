ef2511b74aea0f1902a271c93cc02a8eebf19f61
DerasGertony <denis2006mareev@gmail.com>
2024-10-12 15:21:27 +0300

    test dialogue

--- ld_images.py
+++ ld_images.py
@@ -4,7 +4,9 @@ import pygame
 
 #backs
 startback = pygame.image.load('images/stbc.jpg')
-
+bus = pygame.image.load('Pictures/backgrounds/bus/3.jpg')
+home = pygame.image.load('images/stbc.jpg')
+bed = pygame.image.load('images/stbc.jpg')
 
 #buttons
 sb1 = pygame.image.load('images/stlv1.png')
@@ -13,7 +15,7 @@ sb3= pygame.image.load('images/stlv3.png')
 
 
 #dg window
-dg = pygame.image.load('images/dgfon.png')
+dg = pygame.image.load('images/dgf.png')
 ex = pygame.image.load('images/btexit.png')
 sk = pygame.image.load('images/btfr.png')
 bc = pygame.image.load('images/btbc.png')
