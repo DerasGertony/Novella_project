5f9a65d18be6075636cd4731e2eb388b64af115c
DerasGertony <denis2006mareev@gmail.com>
2024-10-12 14:17:15 +0300

    dialogue windows

--- ld_images.py
+++ ld_images.py
@@ -10,3 +10,10 @@ startback = pygame.image.load('images/stbc.jpg')
 sb1 = pygame.image.load('images/stlv1.png')
 sb2= pygame.image.load('images/stlv2.png')
 sb3= pygame.image.load('images/stlv3.png')
+
+
+#dg window
+dg = pygame.image.load('images/dgfon.png')
+ex = pygame.image.load('images/btexit.png')
+sk = pygame.image.load('images/btfr.png')
+bc = pygame.image.load('images/btbc.png')
