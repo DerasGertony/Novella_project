eee631097e4905c5e7f5fdce247887842f55562a
isOdin <ivanbildagin@mail.ru>
2024-10-16 14:42:15 +0300

    постоянное уменьшение счётчика и через 5 секунд

--- wakeUp.py
+++ wakeUp.py
@@ -7,18 +7,29 @@ btnEventId = {"SPACE": 32}
 FPS = 60
 
 def wakeUp():
-    is_running = True
     clock = pygame.time.Clock()
-    count = 0
+    count = 5
+    time_minusProgress = set()
+    time_minusProgress_after_n_sec = set()
 
+    is_running = True
     while is_running:
-
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
                 pygame.quit()
             if event.type == pygame.KEYDOWN and event.key == btnEventId["SPACE"]:
                 count+=1
+
+        second_now = pygame.time.get_ticks()//1000
+        if second_now > 0 and second_now not in time_minusProgress:
+            count-=1
+            time_minusProgress.add(second_now)
+
+        if second_now > 0 and (second_now)%5 == 0 and second_now not in time_minusProgress_after_n_sec:
+            count-=5
+            time_minusProgress_after_n_sec.add(second_now)
         print(count)
 
         pygame.display.update()
