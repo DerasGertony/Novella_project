885f0f13ddf3d2975efb05aa443aeabc9d68bedd
DerasGertony <denis2006mareev@gmail.com>
2024-10-17 21:48:39 +0300

    added template for plot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 import pygame
 from ld_images import *
+from pyplot2 import *
 
 # Configuration
 pygame.init()
@@ -11,7 +12,7 @@ width, height = 1920, 1080
 pygame.display.set_caption('HSE! STUDENT! LIFE!')
 screen = pygame.display.set_mode((width, height))
 game_started = False
-font = pygame.font.SysFont('Arial', 40)
+font = pygame.font.SysFont('Arial', 30)
 
 
 class Button(pygame.sprite.Sprite):
@@ -53,7 +54,7 @@ dec_sprites = pygame.sprite.Group()
 # starting screen
 back = BackGround(startback)
 st1 = Button([width / 4, height / 2 + 50], 's', font, (0, 0, 0),sb1, (808, 152), (1,1))
-st2 = Button([width/4 +10, height/2 +200], '', font, (0,0,0),sb2,(804, 151), (2,1))
+st2 = Button([width/4 +10, height/2 +200], '', font, (0,0,0),sb2,(804, 151), (2,0,0))
 st3 = Button([width/4, height/2 + 350], '', font, (0,0,0),sb3,(800, 150), (3,1))
 use_sprites.add(st1)
 use_sprites.add(st2)
@@ -64,6 +65,24 @@ butns = {}
 backs = {}
 heroes = {}
 
+def blit_text(surface, text, pos, font, color=pygame.Color('gray')):
+    words = [word.split(' ') for word in text.splitlines()]
+    space = font.size(' ')[0]
+    max_width, max_height = surface.get_size()[0] - 200, surface.get_size()[1]
+    x, y = pos
+    for line in words:
+        for word in line:
+            word_surface = font.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = pos[0]
+                y += word_height
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = pos[0]
+        y += word_height
+
+
 
 def level(num):
     if num == (0,0):
@@ -73,11 +92,15 @@ def level(num):
     pygame.display.flip()
     back = BackGround(bus)
     dec_sprites.add(back)
-    dec_sprites.add(BackGround(dg))
-    use_sprites.add(Button([150, 50], '', font, (0, 0, 0), bc, (300, 80), (num[0], num[1]-1)))
-    use_sprites.add(Button([width - 150, 50], '', font, (0, 0, 0), sk, (300, 80), (num[0],num[1]+1)))
+    dlg = BackGround(dg)
+    dec_sprites.add(dlg)
+
+    use_sprites.add(Button([150, 50], '', font, (0, 0, 0), bc, (300, 80), (num[0], num[1], max(num[2]-1, 0))))
+    use_sprites.add(Button([width - 150, 50], '', font, (0, 0, 0), sk, (300, 80), (num[0],num[1],min(mx2[(num[0],num[1])],num[2]))))
     use_sprites.add(Button([width/2, 60], '', font, (0, 0, 0), ex, (300, 120), (0,0)))
 
+    textpos = (220, 820)
+    blit_text(dlg.image, text2[num], textpos, font)
 
 
 
