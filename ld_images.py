0efa7d9bd1a02db2198f78d5afe93b6499f37914
redukeee <daartemev@edu.hse.ru>
2024-10-23 15:56:48 +0300

    add pictures

--- ld_images.py
+++ ld_images.py
@@ -1,7 +1,6 @@
 import sys
 import pygame
 
-
 # Backs
 startback = pygame.image.load('images/stbc.jpg')
 bus = pygame.image.load('Pictures/backgrounds/bus/3.jpg')
@@ -13,32 +12,39 @@ sb1 = pygame.image.load('images/Group 198.png')
 sb2 = pygame.image.load('images/Group 199.png')
 sb3 = pygame.image.load('images/Group 200.png')
 
-
 # dg window
 dg = pygame.image.load('images/dgf.png')
 ex = pygame.image.load('images/btexit.png')
 sk = pygame.image.load('images/btfr.png')
 bc = pygame.image.load('images/btbc.png')
 
-
 # Heroes
-gm1 = pygame.image.load('Pictures/characters/grandmother_1/main_1.png')
-gm2 = pygame.image.load('Pictures/characters/grandmother_2/main.png')
-qr = pygame.image.load('Pictures/characters/quadrober/main.png')
+gm_good = pygame.image.load('Pictures/characters/кокет бабка.png')
+gm_angry = pygame.image.load('Pictures/characters/злой бабка((.png')
+qr = pygame.image.load('Pictures/characters/квадробер.png')
+gop_normal1 = pygame.image.load('Pictures/characters/гопник 1 нормис.png')
+gop_normal2 = pygame.image.load('Pictures/characters/гопник 2 нормис.png')
+gop_friendly1 = pygame.image.load('Pictures/characters/гопник 1 добри.png')
+gop_friendly2 = pygame.image.load('Pictures/characters/гопник 2 добри.png')
+biv_ild = pygame.image.load('Pictures/characters/биван ильдягин.png')
+ser_depression = pygame.image.load('Pictures/characters/дипрессия козла сергеевича.png')
+sh_friendly = pygame.image.load('Pictures/characters/добри шалья ильявин)).png')
+sh_angry = pygame.image.load('Pictures/characters/злой шалья илявин((.jpg')
+bi_sigma = pygame.image.load('Pictures/characters/игорь борисович сигма с шаурмой.png')
 
 # BackGrounds
-  # BUS
+# BUS
 bus1 = pygame.image.load('Pictures/backgrounds/bus/1.jpg')
 bus2 = pygame.image.load('Pictures/backgrounds/bus/2.jpg')
 bus3 = pygame.image.load('Pictures/backgrounds/bus/3.jpg')
 
-  # HOME
+# HOME
 alarm = pygame.image.load('Pictures/backgrounds/home/alarm.png')
 kitchen = pygame.image.load('Pictures/backgrounds/home/kitchen.png')
 phone = pygame.image.load('Pictures/backgrounds/home/phone.png')
 mainbg = pygame.image.load('Pictures/backgrounds/home/main_back.png')
 
-  # OUTSIDE
+# OUTSIDE
 CrowdedModernStreet = pygame.image.load('Pictures/backgrounds/outside/3.jpg')
 CrowdedStreet = pygame.image.load('Pictures/backgrounds/outside/5.jpg')
 DesertedDarknessStreet = pygame.image.load('Pictures/backgrounds/outside/4.jpg')
